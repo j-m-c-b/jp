39b7cf2255389eb2b6bf56a9389d418c929f4473
j-m-c-b <25614891+j-m-c-b@users.noreply.github.com>
2020-06-25 04:38:31 -0500

    Add files via upload

---
+++ guess/kanji.js
@@ -0,0 +1,57 @@
+kanjiList = [
+    {
+      "S": "一 ",
+      "P": " ichi   hitotsu ",
+      "M": " one   1   first"
+    },
+    {
+      "S": "二 ",
+      "P": " ni   futatsu ",
+      "M": " two   2   second"
+    },
+    {
+      "S": "三 ",
+      "P": " san   mi   mitsu ",
+      "M": " three   3   third"
+    },
+    {
+      "S": "四 ",
+      "P": " yon   shi   yotsu ",
+      "M": " four   4   forth"
+    },
+    {
+      "S": "五 ",
+      "P": " go   itsutsu ",
+      "M": " five   5   fifth"
+    },
+    {
+      "S": "六 ",
+      "P": " roku   mutsu   mui ",
+      "M": " six   6   sixth"
+    },
+    {
+      "S": "七 ",
+      "P": " shichi   nanatsu   nano ",
+      "M": " seven   7   seventh"
+    },
+    {
+      "S": "八 ",
+      "P": " hachi   yatsu   you ",
+      "M": " eight   8   eighth"
+    },
+    {
+      "S": "九 ",
+      "P": " kyuu   ku   kokonotsu ",
+      "M": " nine   9   ninth"
+    },
+    {
+      "S": "十 ",
+      "P": " juu   tou   ju ",
+      "M": " ten   10   tenth"
+    },
+    {
+      "S": "百 ",
+      "P": " hyaku ",
+      "M": " hundred   100"
+    }
+  ]
